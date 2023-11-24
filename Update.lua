43f9a9fce9975b12f2c4eb032f441383e40b7fd4
JinZhi <136086633+Jin-Zhi@users.noreply.github.com>
2023-11-24 20:40:00 +0800

    Update Update.lua

--- Update.lua
+++ Update.lua
@@ -1,7 +1,12 @@
-local version = "1.0.1" --定义本地脚本版本号
-local iscanupdt = gg.makeRequest("https://raw.githubusercontent.com/Jin-Zhi/TiVts/main/update.").content:gsub("%\n$","") --定义云端版本号
-gg.toast("检查更新中 | Checking for updates") --弹出toast对话框
-local downloadfile = gg.getFile():gsub("%.lua$","").."["..iscanupdt.."].lua"--导出路径（因为不可以覆盖脚本只能这样了）
+local iscanupdt,version = "1.0.1" --定义本地脚本版本号
+local iscanupdt_pre = gg.makeRequest("https://raw.githubusercontent.com/Jin-Zhi/TiVts/main/update.") --定义云端版本号链接
+if iscanupdt_pre.content == nil then --如果断网
+    iscanupdt = version --不弹出更新
+else --联网
+    iscanupdt = gg.makeRequest("https://raw.githubusercontent.com/Jin-Zhi/TiVts/main/update.").content:gsub("%\n$","") --定义云端版本号
+    local downloadfile = gg.getFile():gsub("%.lua$","").."["..iscanupdt.."].lua"--导出路径（因为不可以覆盖脚本只能这样了）
+end
+gg.toast("检查更新中") --弹出toast对话框
 local function downloadform(files,text) --定义函数
     local downloadfiles = io.open(files,"w") --使用io库打开文件目录
     if downloadfiles then --如果文件目录存在
@@ -19,5 +24,8 @@ if iscanupdt ~= version then --检查版本如果和云端不符
         local download = gg.makeRequest("https://raw.githubusercontent.com/Jin-Zhi/TiVts/main/Update.lua").content --定义云端脚本链接
         os.exit(downloadform(downloadfile,download)) --退出并导出
     end
+end
+if iscanupdt_pre.content == nil then
+    gg.alert("离线模式")
 end
 print("Hello world!--这是新版本1.0.1") --演示
